f994cdaff22869ee8c904f04d29bf4f457714bd2
Norbert Benczúr <norbert.benczur@gmail.com>
2020-05-02 08:41:11 +0200

    Implemented websocket support

---
+++ client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			// log.Fatal("Error: client read", err)
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			// log.Fatal("Error: client write", err)
+			return
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nukesz/go-chat
 
 go 1.12
+
+require github.com/gorilla/websocket v1.4.2
